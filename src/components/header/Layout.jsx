6a28ebfe9fef85d369a1e6a4a6b6ab7a13fae853
matvey84 <matvey8407@mail.ru>
2022-09-19 21:54:33 +0300

    feat: add aplication

--- src/components/header/Layout.jsx
+++ src/components/header/Layout.jsx
@@ -3,16 +3,16 @@ import { Link, Outlet } from "react-router-dom"
 
 export function Layout(){
 
-	return(
-	<>
-		<div className='nav-links'>
-			<Link className='link' to="/"> На главную</Link>
-			{/* <Link className='link' to="searchResult"/> */}
-			{/* <Link className='link' to={`/main/${}/${}/${}`}/> */}
-			<Link className='link' to="/*"/>
-		</div>
-		<Outlet/>
-	</>
+  return(
+  <>
+    <div className='nav-links'>
+      <Link className='link' to="/"> На главную</Link>
+      {/* <Link className='link' to="searchResult"/> */}
+      {/* <Link className='link' to={`/main/${}/${}/${}`}/> */}
+      <Link className='link' to="/*"/>
+    </div>
+    <Outlet/>
+  </>
 )
 }
 
--- src/components/movieItems/MovieItemsList.jsx
+++ src/components/movieItems/MovieItemsList.jsx
@@ -2,23 +2,18 @@ import React from "react";
 import MovieItem from "./MovieItems";
 import {nanoid} from 'nanoid';
 
-
-
 export default function MovieItemsList (props){
-
-return(
-		<>
-			{ props.data.Search.map((items,i)=>
-					<MovieItem
-						id={i}
-						key={nanoid()}
-						data={props.data}
-					  getFetchForDetailsInfo={props.getFetchForDetailsInfo}
-
-					
-					/>
-				)
-			}
-		</>
-	)
+  return(
+    <>
+      { props.data.Search.map((items,i) =>
+          <MovieItem
+            id={i}
+            key={nanoid()}
+            data={props.data}
+            getFetchForDetailsInfo={props.getFetchForDetailsInfo}
+          />
+        )
+      }
+    </>
+  )
 }
